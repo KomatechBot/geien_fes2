ec96005654ba57d5169ab27a88b8aa9badf4fb43
Rikuxx0 <rikum0730@gmail.com>
2025-06-19 15:32:45 +0900

    main

--- src/app/api/exhibitions/[id]/route.ts
+++ src/app/api/exhibitions/[id]/route.ts
@@ -1,13 +1,8 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { client } from '@/lib/microcms' // microCMSクライアントを定義した場所
 
-type Params = {
-  params: {
-    id: string
-  }
-}
 
-export async function GET(req: NextRequest, { params }: Params) {
+export async function GET(req: NextRequest, { params }: { params: Promise<{ id: string }>}) {
    const { id } = await params
 
     const data = await client.get({
